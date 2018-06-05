08853f1e70c12cdbd7cc6fcb0b26b3102b32170c
Cody Soyland <cody@soyland.org>
2018-06-05 17:22:45 -0500

    Add flag for bind address

--- cmd/pilosa-webui/main.go
+++ cmd/pilosa-webui/main.go
@@ -1,23 +1,41 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"regexp"
 
+	"github.com/pkg/errors"
 	"github.com/rakyll/statik/fs"
 
 	_ "github.com/pilosa/webui/pkg/static/statik"
 )
 
+var bind = flag.String("bind", ":8000", "bind address")
+
 func main() {
+	var port string
+	flag.Parse()
+	re := regexp.MustCompile(".*:(\\d{1,5})$")
+	match := re.FindStringSubmatch(*bind)
+	if len(match) == 2 {
+		port = match[1]
+	} else {
+		log.Fatalf("Invalid bind address: %s", *bind)
+	}
+	webAddress := "http://localhost:" + port
 
 	statikFS, err := fs.New()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(errors.Wrap(err, "initializing statik filesystem"))
 	}
 
 	http.Handle("/", http.FileServer(statikFS))
 
-	log.Println("Serving Pilosa WebUI at http://localhost:8000")
-	http.ListenAndServe(":8000", nil)
+	log.Println("Serving Pilosa WebUI at " + webAddress)
+	err = http.ListenAndServe(*bind, nil)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "running ListenAndServe"))
+	}
 }
